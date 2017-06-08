1f3be8f3d8f85b04bcbeec432a162b9aaa55c4b2
nfour <novus.nfour@gmail.com>
2017-06-08 17:45:51 +1000

    Re-ordering

--- tslint.js
+++ tslint.js
@@ -8,21 +8,12 @@ module.exports = {
     /** Lock down indentation */
     "ter-indent": [true, 2],
 
-    /** Consistancy fixes */
-    "ter-func-call-spacing": [true],
-    "curly": true,
-    "trailing-comma": [true, { "multiline": "always", "singleline": "never" }],
-    "object-literal-key-quotes": [ true, "consistent-as-needed" ]
+    /** Typescripts vscode tmlanguage actually needs semicolons ;( */
+    "semicolon": [true, "always"],
     
     /** Using ter's line length to ignore comments */
     "max-line-length": [false],
     "ter-max-len": [true, 130, { ignoreComments: true }],
-
-    /** Typescripts vscode tmlanguage actually needs semicolons ;( */
-    "semicolon": [true, "always"],
-    "object-curly-spacing": [ true, "always" ],
-    "array-bracket-spacing": [true, "never"],
-    "space-in-parens": [true, "never"],
     
     /** Laxer tslint */
     "no-angle-bracket-type-assertion": false,
@@ -30,5 +21,14 @@ module.exports = {
 
     /** Disabled, because pointless */
     "object-literal-sort-keys": false,
+
+    /** Aesthetic & consistancy */
+    "ter-func-call-spacing": [true],
+    "curly": true,
+    "trailing-comma": [true, { "multiline": "always", "singleline": "never" }],
+    "object-literal-key-quotes": [ true, "consistent-as-needed" ]
+    "object-curly-spacing": [ true, "always" ],
+    "array-bracket-spacing": [true, "never"],
+    "space-in-parens": [true, "never"],
   }
 }
